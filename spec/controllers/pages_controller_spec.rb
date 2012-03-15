f14d3a85098950c09896189aa26fb1399c67aa8e
Gino Garcia <ginogarcia@netzero.net>
2012-03-15 15:43:22 -0500

    modify the pages_controller_spec.rb

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,11 +2,6 @@ require 'spec_helper'
 
 describe PagesController do
 
-  describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
-  end
+ 
 
 end
